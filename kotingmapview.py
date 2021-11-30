51d5a73e9f5170727627526f9a38179e4477fedc
Einfallslos03 <94938072+Einfallslos03@users.noreply.github.com>
2021-11-30 23:19:27 +0100

    Add files via upload

---
+++ kotingmapview.py
@@ -0,0 +1,17 @@
+from kivy_garden.mapview import MapView
+from kivy.clock import Clock
+
+class KotingMapView(MapView):
+
+    getting_markets_timer = None
+
+    def start_getting_markets_in_fov(self):
+        try:
+            self.getting_markets_timer.cancel()
+        except:
+            pass
+
+        self.getting_markets_timer = Clock.schedule_once(self.get_markets_in_fov, 1)
+    
+    def get_markets_in_fov(self, *args):
+        print(self.get_bbox())
